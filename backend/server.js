52b9f4cf2143c96356977c3fc7cf6bc88f367670
Mas Rama <ryushineo@gmail.com>
2024-05-28 05:57:40 +0700

    setting up env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+if( !process.env.NODE_ENV ) {
+  throw new Error('ERROR: NODE_ENV IS MISSING')
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
