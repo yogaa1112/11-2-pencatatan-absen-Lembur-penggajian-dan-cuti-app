99e1e0cb0d19be591a6a478fd9ed54886112ad26
Mas Rama <ryushineo@gmail.com>
2024-05-28 08:19:34 +0700

    add middleware and dev scripts

---
+++ backend/middleware/logger.js
@@ -0,0 +1,3 @@
+const morgan = require('morgan');
+const logger = morgan('dev');
+module.exports = logger;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const Router = require('./routes/index');
-
+const Logger = require('./middleware/logger');
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +10,15 @@ if( !process.env.NODE_ENV ) {
   throw new Error('ERROR: NODE_ENV IS MISSING')
 }
 
+// Use built-in middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Use custom middleware
+app.use(Logger)
+
+//main routes
 app.use(Router);
 
 
