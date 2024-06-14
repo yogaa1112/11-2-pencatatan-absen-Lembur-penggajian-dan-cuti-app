2ad9566296535a581282597e7137f2fd4b0e71d0
Muhammad Ichsan Dedi Aprivian <muhammadichsanda86@gmail.com>
2024-06-14 15:46:10 +0700

    update ProtectedRoute.js

---
+++ frontend/src/Components/ProtectedRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const token = localStorage.getItem('token');
+  
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        token ? (
+          <Component {...props} />
+        ) : (
+          <Navigate to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
