b065081c67bffb5eb3bbfa693d103e5562fad26e
Mas Rama <ryushineo@gmail.com>
2024-06-18 07:04:18 +0700

    fix element rendered twice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@ import EmployeePersonal from "./Components/EmployeePersonal";
 import EmployeeStaff from "./Components/EmployeeStaff";
 import EmployeePayroll from "./Components/EmployeePayroll";
 import AddSalary from "./Components/AddSalary";
+import Salary from "./Components/Salary"
+import Attendance from "./Components/Attendance";
 import Leave from "./Components/Leave";
 import User from "./User";
 import Logout from "./Components/Logout";
@@ -45,25 +47,32 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        <Route path="/admin/*" element={<> <ProtectedRoute /> <Admin/> </>}>
-          <Route path="home" element={<Admin />} />
-          <Route path="employees" element={<Employees />} />
-          <Route path="add-employee" element={<EmployeePersonal />} />
-          <Route path="employee-staff" element={<EmployeeStaff />} />
-          <Route path="employee-payroll" element={<EmployeePayroll />} />
-          <Route path="add-salary" element={<AddSalary />} />
+        <Route path="/admin/*" element={ <ProtectedRoute /> }>
+          <Route path="" element={<Admin />}>
+            <Route path="home" element={<Admin />} />
+            <Route path="employees" element={<Employees />} />
+            <Route path="add-employee" element={<EmployeePersonal />} />
+            <Route path="employee-staff" element={<EmployeeStaff />} />
+            <Route path="employee-payroll" element={<EmployeePayroll />} />
+            <Route path="attendance" element={<Attendance />} />
+            <Route path="salary" element={<Salary />} />
+            <Route path="add-salary" element={<AddSalary />} />
+            <Route path="logout" element={<Logout />} />
+          </Route>
         </Route>
 
         
 
-        <Route path="/user/*" element={<> <ProtectedRoute /> <User/> </>}>
-          <Route path="userhome" element={<User />} />
-          <Route path="userattendance" element={<UserAttendance />} />
-          <Route path="usersalary" element={<UserSalary />} />
-          <Route path="userleave" element={<Leave />} />
-          <Route path="userattendancein" element={<UserAttendanceIn />} />
-          <Route path="attendanceout" element={<AttendanceOut />} />
-          <Route path="logout" element={<Logout />} />
+        <Route path="/user/*" element={<ProtectedRoute />}>
+          <Route path="" element={<User/>}>
+            <Route path="userhome" element={<User />} />
+            <Route path="userattendance" element={<UserAttendance />} />
+            <Route path="usersalary" element={<UserSalary />} />
+            <Route path="userleave" element={<Leave />} />
+            <Route path="userattendancein" element={<UserAttendanceIn />} />
+            <Route path="userattendanceout" element={<AttendanceOut />} />
+            <Route path="logout" element={<Logout />} />
+          </Route>
         </Route>
 
         <Route path="*" element={<Navigate to="/" />} />
